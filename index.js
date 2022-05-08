0e48a7cf8a36337946a2d0489f10dfe5104f2b85
Eva Reitzel <evareitzel@gmail.com>
2022-05-08 18:13:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1"); 
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "YOUR-NAME is the champion"
